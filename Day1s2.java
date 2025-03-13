3e6d36a2c43a8a0242d327da38c95423058ff42c
Yabgun <bugrattumlu@gmail.com>
2025-03-13 20:32:23 +0300

    Day 1 solve 2: Scanner

---
+++ Day1s2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Day1s2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        int num3 = scanner.nextInt();
+        
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        
+        scanner.close();
+    }
+}
