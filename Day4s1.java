549cb3f2816b6fa66c47e9da61bb31b7d7ad855f
Yabgun <bugrattumlu@gmail.com>
2025-03-16 22:07:04 +0300

    Day 4 solve 1: Loops

---
+++ Day4s1.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Day4s1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        for (int i = 1; i <= 10; i++) {
+            int result = N * i;
+            System.out.println(N + " x " + i + " = " + result);
+        }
+
+        bufferedReader.close();
+    }
+}
