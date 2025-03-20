15683109d766bfed6c9c1182c1118bd549a092d0
Yabgun <bugrattumlu@gmail.com>
2025-03-20 21:15:35 +0300

    Day 7 solve 1: EOF

---
+++ Day7s1.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Day7s1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        try {
+            int lineNumber = 1;
+            
+            // Scanner.hasNext() metodu EOF'a ulaşana kadar devam eder
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                
+                // Satır numarası, bir boşluk ve satır içeriğini yazdır
+                System.out.println(lineNumber + " " + line);
+                
+                // Satır numarasını artır
+                lineNumber++;
+            }
+        } finally {
+            scanner.close();
+        }
+    }
+} 
